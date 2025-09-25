c78beabbb7af818d6d2ccee94238ff3b3b4b7be7
Earl <earl.dozier@sjsu.edu>
2025-09-24 17:10:20 -0700

    Added Inventory class structure

--- src/main/java/rpg/items/Inventory.java
+++ src/main/java/rpg/items/Inventory.java
@@ -1,1 +1,37 @@
 package rpg.items;
+
+public class Inventory {
+
+    private int slotCapacity;
+    private List<Items> items;
+
+    public Inventory(int slotCapacity) {
+        this.slotCapacity = slotCapacity;
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(Item item) throws InventoryFullException {
+
+    }
+
+    public void removeItem(int itemId) {
+
+    }
+
+    public boolean hasSpaceFor(Item item) {
+
+    }
+
+    public Item findById(int itemId) {
+
+    }
+
+    public double totalWeight() {
+
+    }
+
+    @Override
+    public String toString() {
+
+    }
+}
