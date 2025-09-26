8b93ab609dbfd983b30dcaa6709c719c930b27b5
Welson Kuang <welson.kuang@sjsu.edu>
2025-09-26 02:50:26 -0700

    Worked on README.md, PlayableCharacter, Enemy, Ability

--- src/main/java/rpg/characters/Enemy.java
+++ src/main/java/rpg/characters/Enemy.java
@@ -1,1 +1,37 @@
-package rpg.core;
+package rpg.characters;
+
+import java.util.List;
+import rpg.items.Item;
+import rpg.characters.Characters;
+import rpg.abilities.Ability;
+
+public class Enemy extends Characters {
+
+    // Methods
+
+    public boolean fleeChance() {}
+
+    public void respawn() {}
+
+    public List<Item> dropLoot() {}
+
+    public void applyStatusEffect(Characters target) {}
+
+    // Overrides due to Characters
+
+    @Override
+    public void attack(Characters target) {}
+
+    @Override
+    public void takeDamage(int amt) {}
+
+    @Override
+    public void useAbility(Ability ability, Characters target) {}
+
+    @Override
+    public void startTurn() {}
+
+    @Override
+    public boolean canAct() {}
+
+}
--- src/main/java/rpg/characters/PlayableCharacter.java
+++ src/main/java/rpg/characters/PlayableCharacter.java
@@ -1,1 +1,37 @@
-package rpg.core;
+package rpg.characters;
+
+import java.util.List;
+import rpg.items.Weapon;
+import rpg.items.Item;
+import rpg.abilities.Ability;
+
+public class PlayableCharacter extends Characters {
+
+    // Methods
+
+    public void heal(int amount) {}
+
+    public void levelUp() {}
+
+    public public void equipWeapon(Weapon weapon) {}
+
+    public void loot(List<Item> items) {}
+
+    // Overrides due to Characters
+
+    @Override
+    public void attack(Characters target) {}
+
+    @Override
+    public void takeDamage(int amt) {}
+
+    @Override
+    public void useAbility(Ability ability, Characters target) {}
+
+    @Override
+    public void startTurn() {}
+
+    @Override
+    public boolean canAct() {}
+
+}
