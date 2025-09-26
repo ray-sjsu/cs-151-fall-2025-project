b02a39b27964984a9e6c29c360777e4577178407
Welson Kuang <welson.kuang@sjsu.edu>
2025-09-25 21:57:26 -0700

    Added Ability.java and worked on Characters, ActionType, StatType

--- src/main/java/rpg/characters/Characters.java
+++ src/main/java/rpg/characters/Characters.java
@@ -1,1 +1,35 @@
 package rpg.characters;
+
+import java.util.List;
+import java.util.Map;
+import rpg.items.Inventory;
+import rpg.abilities.Ability;
+
+public abstract class Characters {
+    private String name;
+    private int characterId;
+
+    private String description;
+    private int level;
+    private int healthPoints;
+    private int actionPoints;
+
+    private Inventory inventory;
+    private List<Ability> abilities;
+    private Map<StatType, Integer> stats;
+    private StatusType status;
+
+    // Methods
+
+    public abstract void attack(Characters target);
+    public abstract void takeDamage(int amt);
+    public abstract void useAbility(Ability ability, Characters target) throws AbilityOnCooldownException;
+    public abstract void startTurn();
+    public abstract boolean canAct();
+
+    @Override
+    public abstract String toString();
+
+    public abstract int getStat(StatType type);
+    public abstract int setStat(StatType type, int value);
+}
