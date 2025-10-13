db1b5ca0f0b985fe3fe1a7ea0ac30661d131475e
ray-sjsu <raymund.mercader@sjsu.edu>
2025-10-13 12:01:48 -0700

    Refactor Weapon to properly use Usable Interface + Refactor Characters for method name change

--- src/main/java/rpg/items/Weapon.java
+++ src/main/java/rpg/items/Weapon.java
@@ -2,8 +2,10 @@ package rpg.items;
 
 import rpg.characters.Characters;
 import rpg.core.RarityType;
+import rpg.core.StatusType;
+import rpg.core.Usable;
 
-public class Weapon extends Item {
+public class Weapon extends Item implements Usable {
     private final int minDamage;
     private final int maxDamage;
     private final double critChance;
@@ -15,10 +17,14 @@ public class Weapon extends Item {
         this.critChance = critChance;
     }
 
-    public int calculateDamageWithCriticalChance(Characters user, Characters target) {
+    public int use(Characters user, Characters target) {
         int base = (int) (Math.random() * (maxDamage - minDamage + 1)) + minDamage;
         boolean critical = Math.random() < critChance;
         return critical ? base * 2 : base;
+    }
+
+    public boolean isUsable(Characters user, Characters target) {
+        return user.canAct() && (target.getStatus() != StatusType.MISSING || target.getStatus() != StatusType.DEAD);
     }
 
     @Override
